446775bdf40b9c71ca18212fac8051973a2a3f22
khardiataBasse <83843803+khardiataBasse@users.noreply.github.com>
2023-05-05 15:51:21 +0000

    Add files via upload

---
+++ Secretaire.java
@@ -0,0 +1,14 @@
+public class Secretaire extends Personne {
+
+   
+        public Secretaire(){};
+        public Secretaire(String nom, String prenom, int age){
+            super(nom, prenom, age);
+
+        }
+            public void gererRendezvous(){
+                 
+            }
+        
+    
+}
